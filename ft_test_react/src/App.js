ea916c6166ce5896102988d163eefacfacb59776
Bishnu Bhattarai <bishnu@tekkon.io>
2019-12-21 13:47:19 +0545

    react setup first step change

---
+++ ft_test_react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {ShowResultsFromAPI} from './ReactComponentExercise'
+
+function App() {
+  return (
+    <ShowResultsFromAPI></ShowResultsFromAPI>
+  );
+}
+
+export default App;
